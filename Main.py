ac979d84a3b0d7d80643b9821b2ec14e483bc968
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-05-28 12:23:02 -0700

    Initial commit...
    Added the list sorter at time = list squared.

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-
+def List_Sorter_main():
+    print("Enter your list!")
+    a = 0
+    print("Input the length of the list.")
+    while a < input():
+        lst.append(input("list[", a, "]:"))
+    dictionary = {}
+    for a in lst:
+        for b in lst:
+            if a > b:
+                c += 1
+        dictionary[a] = c
+    print(dictionary)
+    a = 0
+    newlst = []
+    while a < len(lst):
+        newlst.append(0)
+        a += 1
+    for a in dictionary:
+        newlst[dictionary.get(a)] = a
+    print(newlst)
+    return newlst
