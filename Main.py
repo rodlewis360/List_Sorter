a4c508d9e54f923168acd4f2617bdfa38a5aff49
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-05-28 15:17:09 -0700

    Bug fixes
    initial bug fixes such as THE LIST ISN'T REGISTERED!

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 def List_Sorter_main():
-    print("Enter your list!")
+    print("Enter your list using the steps bellow!")
     a = 0
+    import time
+    time.sleep(1)
     print("Input the length of the list.")
-    while a < input():
-        lst.append(input("list[", a, "]:"))
+    length = int(input())
+    while a < length:
+        lst.append(int(input("list[", a, "]: ")))
+        time.sleep(0.5)
+        a += 1
     dictionary = {}
     for a in lst:
         for b in lst:
